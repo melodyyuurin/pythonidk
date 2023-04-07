3910af3875c85c7025c256e80c53880c04dfec31
just me <88276514+melodyyuurin@users.noreply.github.com>
2023-04-07 12:03:35 +0700

    Update datainput.py

--- datainput.py
+++ datainput.py
@@ -1,5 +1,21 @@
 name = input("whats your name? ")
-age = input("how old are you? ")
-color = input("whats your favortie color? ")
+print("Cool name !")
 
+while True:
+    age = input("how old are you? ")
+    try: 
+        age =int(age)
+        break
+    except ValueError:
+        print("please a valid number")
+
+colorArray = ["red", "green", "blue", "yellow", "white", "black", "pink", "purple", "violet", "brown", "cyan", "orange"]
+while True: 
+    color = input("whats your favortie color? ")
+    if color in colorArray:
+        print("nice.")
+        break
+    else:
+        print("is that a color? please enter a valid color")
+        
 print(f"Hi, {name}! so youre {age} and you like {color}. Nice to meet you!")
