7f1055b04f8f1fc966d79d8ba1fccb934142050c
just me <88276514+melodyyuurin@users.noreply.github.com>
2023-04-07 11:44:36 +0700

    Add files via upload

---
+++ datainput.py
@@ -0,0 +1,5 @@
+name = input("whats your name? ")
+age = input("how old are you? ")
+color = input("whats your favortie color? ")
+
+print(f"Hi, {name}! so youre {age} and you like {color}. Nice to meet you!")
